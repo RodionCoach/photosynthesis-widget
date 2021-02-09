b4086b6edff85f4d7726951a28cb2ee69983e539
Rodion <www.rodionkochetkow@gmail.com>
2021-02-09 16:18:54 +0300

    photosynthesis

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import { Timeline } from "./plugins/timeline";
+import "./index.css";
+
+const popup = document.getElementById("timeline-wrapper");
+const points = document.getElementsByClassName("point");
+let timeline;
+
+const seqCount = {
+  "Photosynthesis_animals": 975,
+  "Photosynthesis_plants": 798,
+}
+
+const showVideo = (id) => {
+  popup.classList.add("opened");
+
+  timeline = new Timeline({
+    containerId: "timeline",
+    namePattern: id,
+    fileExtension: "jpg",
+    framesCount: seqCount[id],
+    framesFolder: `/static/${id}`,
+    fps: 15
+  });
+
+  timeline.init();
+};
+
+const closePopUp = () => {
+  if (popup.classList.contains("opened")) {
+    popup.classList.remove("opened");
+    timeline.destroy();
+  }
+}
+
+for (let point of points) {
+  point.addEventListener("click", () => {
+    showVideo(point.getAttribute("data-point"))
+  });
+}
+
+document.getElementById("closeButton").addEventListener("click", () => {
+  closePopUp();
+});
